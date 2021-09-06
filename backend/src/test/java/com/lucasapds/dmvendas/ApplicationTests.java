3f2a32db8d51761e9b561e48eb48cb82e6ce3e62
Lucas Ap Santos <lucasapdsantos@gmail.com>
2021-09-06 11:28:44 -0300

    Project Created

---
+++ backend/src/test/java/com/lucasapds/dmvendas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasapds.dmvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
